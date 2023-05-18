c2b5bd47ad42aabed81fbb14865cb68b3d328609
Siiiiinth <60766886+Siiiiinth@users.noreply.github.com>
2023-05-18 23:40:49 +0200

    done

--- Graphics_Engine/Hrtf.cpp
+++ Graphics_Engine/Hrtf.cpp
@@ -3,12 +3,18 @@
 
 Hrtf::Hrtf()
 {
+	m_options = SAF_SOFA_READER_OPTION_NETCDF;
 }
 
 Hrtf::~Hrtf()
 {
 }
 
-void Hrtf::LoadSOFA(std::string* file)
+void Hrtf::LoadSOFA(char* file)
 {
+	saf_sofa_container sofa;
+
+	//SAF_SOFA_ERROR_CODES error = saf_sofa_open(&sofa, file, m_options);
+	//std::cout << error << std::endl;
+	
 }
--- Graphics_Engine/Hrtf.h
+++ Graphics_Engine/Hrtf.h
@@ -2,11 +2,11 @@
 
 
 #define SAF_USE_INTEL_MKL_LP64
-#define SAF_ENABLE_SIMD 
-#define SAF_USE_FFTW
 #define SAF_ENABLE_SOFA_READER_MODULE
 
 #include "saf.h"
+#include "saf_externals.h"
+#include <iostream>
 #include <string>
 
 
@@ -17,8 +17,9 @@ public:
 	Hrtf();
 	~Hrtf();
 
-	void LoadSOFA(std::string* file);
+	void LoadSOFA(char* file);
 
 private:
-
+	saf_sofa_container m_hSOFA;
+	SAF_SOFA_READER_OPTIONS m_options;
 };
